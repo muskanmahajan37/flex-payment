533b883c91635caa4a33905d391648d570500d10
Rron <rronjk@gmail.com>
2020-05-31 10:34:32 +0200

    Project setup

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const cors = require("cors");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+
+const app = express();
+
+mongoose.connect(
+  `mongodb+srv://rronjakupi:${process.env.MONGO_ATLASS_PW}@${process.env.DATABASE_NAME}-ny5dc.mongodb.net/test?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  }
+);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+
+const PORT = process.env.port || 8004;
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
