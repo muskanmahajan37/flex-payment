fd097a78a85c28adcb25f247cebbfe9c216a3f98
Rron <rronjk@gmail.com>
2020-06-24 19:47:05 +0200

    Added route delete for handling payment deletion

--- api/routes/PaymentRouter.js
+++ api/routes/PaymentRouter.js
@@ -6,5 +6,6 @@ const PaymentController = require('../controllers/PaymentController');
 router.get('/', PaymentController.getAllPayments);
 router.get('/:paymentId', PaymentController.getPaymentsByID);
 router.post('/', PaymentController.makePayment);
+router.delete('/:paymentId', PaymentController.deletePayment);
 
 module.exports = router;
